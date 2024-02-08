c3b8a7b1640c94d7b99ffff569608839fae4fea5
Joe <ckdfo1995@gmail.com>
2024-02-08 15:48:18 +0900

    remove funs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function addURL() {
-  return true;
-}
-// 컨테이너 id
-function print() {
-  console.log("log for client");
-}
-function reqestConvert() {
-  // http://localhost:8080/fromyaml
-  return false;
-}
-
 function App() {
   return (
     <div className="App">
